c0b96c0cd1098a2e50630893fb143b93e956f7c5
zzcipod <jonzzheng@gmail.com>
2022-07-30 23:09:40 -0700

    almost done

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
 // GET Route for retrieving all the tips
 notes.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
-
-// Import custom middleware, "cLog"
-app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
